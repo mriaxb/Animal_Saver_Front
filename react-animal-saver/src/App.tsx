8c48d605ec73db3e1108cdd2e1c3c80abc404827
jeann <jeannguilherme@hotmail.com>
2023-05-19 18:11:32 -0300

    criado rotas a partir da página Home, para pets, fornecedores e protetores

--- react-animal-saver/src/App.tsx
+++ react-animal-saver/src/App.tsx
@@ -1,12 +1,24 @@
 
-import { Header } from "./components/header"
+import { Pets } from "./components/Pets"
+import { Fornecedor } from "./components/fornecedores"
+import { Home } from "./components/home"
+import { Protetor } from "./components/protetores/protetores"
 import './global.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+ 
 
 function App() {
 
   return (
     <>
-      <Header/>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/pets" element={<Pets/>}/>
+          <Route path="/fornecedor" element={<Fornecedor/>}/>
+          <Route path="/protetor" element={<Protetor/>}/>
+        </Routes>
+      </Router>
     </>
   )
 }
