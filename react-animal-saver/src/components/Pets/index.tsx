79a4fea60b4ca9db6381f9fac4b78ec18c2ebd72
jeann <jeannguilherme@hotmail.com>
2023-06-10 20:14:49 -0300

    ajustes no footer e service

--- react-animal-saver/src/components/Pets/index.tsx
+++ react-animal-saver/src/components/Pets/index.tsx
@@ -1,5 +1,10 @@
+import { Col, Row } from 'react-bootstrap';
+import styles from './index.modules.css'
+
 export const Pets = () => {
     return (
-            <h1>Área do pet</h1>
+            <>
+             <h1>Área do pet</h1>
+            </>
     )
 }
